b06383c467edca14859878b8c9565c8dcdf73edc
mekanb <baymyradov.mekanx@gmail.com>
2026-01-15 19:48:03 +0500

    🔧 Configure project settings

---
+++ app/core/config.py
@@ -0,0 +1,60 @@
+import secrets
+import warnings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import (
+    PostgresDsn,
+    computed_field,
+    model_validator
+)
+from typing import Literal
+from typing_extensions import Self
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+    API_V0_STR: str = "/api/v0"
+    SECRET_KEY: str = secrets.token_hex(32)
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    PROJECT_NAME: str
+    POSTGRES_HOST: str 
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str 
+
+    @computed_field
+    @property
+    async def ASYNCPG_DATABASE_URI(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            path=self.POSTGRES_DB
+        )
+    
+    def _check_default_secret(self, var_name: str, value: str | None) -> None:
+        if value == "changethis":
+            message = (
+                f'The value of {var_name} is "changethis" ',
+                "Please change it for security, at least for deployments."
+            )
+            if self.ENVIRONMENT == "local":
+                warnings.warn(message)
+            else:
+                raise ValueError(message)
+            
+    @model_validator(mode="after")
+    def _enforce_non_default_secrets(self) -> Self:
+        self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
+        self._check_default_secret("POSTGRES_PASSWORD", self.POSTGRES_PASSWORD)
+
+        return self
+
+
+
+settings = Settings()
